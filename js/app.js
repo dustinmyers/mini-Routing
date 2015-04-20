90b305604bc26ceb1ba1e6685e0583a5b1305b07
Dustin Myers <dustint314@gmail.com>
2015-04-20 10:36:30 -0600

    finished

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-var app = angular.module('miniRouting', ['ngRoute']);
+var app = angular.module('routingApp', ['ngRoute']);
+
+app.config(function($routeProvider) {
+
+	$routeProvider
+		.when('/', {
+			templateUrl: 'js/home/homeTmpl.html',
+			controller: 'homeCtrl.js'
+		})
+
+		.when('/settings', {
+			templateUrl: 'js/settings/settingsTmpl.html',
+			controller: 'settingsCtrl'
+		})
+
+		.when('/products/:id', {
+			templateUrl: 'js/products/productsTmpl.html',
+			controller: 'productsCtrl'
+		})
+
+		.otherwise({
+			redirectTo: '/'
+		})
+});
---
+++ js/products/productsCtrl.js
@@ -0,0 +1,10 @@
+var app = angular.module('routingApp');
+
+app.controller('productsCtrl', function($scope, $routeParams, productService){
+	if ($routeParams.id === 'shoes') {
+		$scope.productData = productService.shoeData;
+	} else if ($routeParams.id === 'socks') {
+		$scope.productData = productService.sockData
+	}
+
+})
